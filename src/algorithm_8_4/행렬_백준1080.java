01cfa459cf2483323f090a48f72be4f9fb016337
한지현 <gksgpals96@naver.com>
2020-08-25 20:56:09 +0900

    #치킨배달 백준 15686

---
+++ src/algorithm_8_4/행렬_백준1080.java
@@ -0,0 +1,10 @@
+package algorithm_8_4;
+
+public class 행렬_백준1080 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/algorithm_WS/치킨배달_백준15687_bitMaching.java
@@ -0,0 +1,84 @@
+package algorithm_WS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class 치킨배달_백준15687_bitMaching {
+	static int N, M;
+	static int Ans = Integer.MAX_VALUE;
+	static int[] sel;
+	
+	static ArrayList<Point> home = new ArrayList<>();
+	static ArrayList<Point> chicken = new ArrayList<>();
+
+	static class Point {
+		int r, c;
+
+		Point(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		sel = new int[M];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				int n = sc.nextInt();
+				if (n == 1) {
+					home.add(new Point(i, j));
+				} else if (n == 2) {
+					chicken.add(new Point(i, j));
+				}
+			}
+		}
+
+		// 전체 치킨 가게 중에 M개의 갯수로 이루어진 조합을 구해
+		// 조합의 치킨 가게와 집들의 최소 거리의 합
+		combination(0, 0,0);
+
+		System.out.println(Ans);
+	}
+
+	private static void combination(int idx, int k, int flag) {
+		if (k == M) {
+			// 다 골랐어요
+//			System.out.println(Arrays.toString(sel));
+			// 집과 골라진 치킨집들의 거리를 계산하여 그 중 가장 짧은 거리들의 합을 구합니다.
+			int sum = 0;
+			for (int i = 0; i < home.size(); i++) {
+				// |r1.r-r2.r|+|r1.c-r2.c|
+				int dist = Integer.MAX_VALUE;
+				for (int j = 0; j < M; j++) {
+					dist = Math.min(dist, cal(home.get(i), chicken.get(sel[j])));
+				}
+				sum += dist;
+			}
+			Ans = Math.min(Ans, sum);
+			return;
+		}
+		if (idx == chicken.size()) {
+			// 더 고를게 없어요
+			return;
+		}
+
+		for (int i = idx; i < chicken.size(); i++) {
+			if((flag & 1<<i)!=0) continue;
+			sel[k] = i;
+			combination(i, k + 1,flag|(1<<i));
+
+		}
+	}
+
+	private static int cal(Point point, Point point2) {
+		return (Math.abs(point.r - point2.r)) + (Math.abs(point.c - point2.c));
+	}
+}
