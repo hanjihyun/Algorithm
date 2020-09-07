db3bf1cd911419db7e0c5c4383bde8ca229fd566
한지현 <gksgpals96@naver.com>
2020-09-07 23:31:34 +0900

    algorithm

--- src/algorithm_IM_Review/스도쿠검증_SWEA1974.java
+++ src/algorithm_IM_Review/스도쿠검증_SWEA1974.java
@@ -20,25 +20,31 @@ public class 스도쿠검증_SWEA1974 {
 			map = new int[9][9];
 			num = new int[10];
 			Ans = 0;
+			//test_case 반복을 위한 초기화 필수
 			row = false;
 			col = false;
 			room = false;
 			for (int i = 0; i < 9; i++) {
+				//num배열 재활용을 위한 초기화
 				num = new int[10];
 				for (int j = 0; j < 9; j++) {
 					map[i][j] = sc.nextInt();
+					//1. 행단위 체크(가로방향): 0,0/0,1/0,2/0,3.. 1,0/1,1/1,2...
 					num[map[i][j]] = 1;
 					
 				}
+				//1~9번 으로 num배열은 10칸으로 잡고 인덱스 1부터 사용
 				for (int k = 1; k <= 9; k++) {
+					//num배열에 1이 아닌 숫자가 있으면 1-9 중에 없는 숫자가 있다는 뜻!
 					if(num[k] != 1) {
 						row = true;
 						break;
 					}
 				}
 			}
-			
+			//2. 열단위 체크 (세로방향):0,0/1,0/2,0....
 			for (int i = 0; i < 9; i++) {
+				//num배열 재활용을 위한 초기화
 				num = new int[10];
 				for (int j = 0; j < 9; j++) {
 					num[map[j][i]] = 1;	
@@ -50,18 +56,19 @@ public class 스도쿠검증_SWEA1974 {
 					}
 				}
 			}
-		
+			//3. 작은 4각형(9개) 체크
 			for (int i = 0; i < 3; i++) {
-				
 				for (int j = 0; j < 3; j++) {
+					//num배열 재활용을 위한 초기화
 					num = new int[10];
-					for (int k = 0; k < 3; k++) {
+					//작은 4각형 1개처리 i->0, 0,1,2 / i->1, 3,4,5 /i->2, 6,7,8
+					for (int k = 0; k < 3; k++) {		
 						for (int l = 0; l < 3; l++) {
 							num[map[i*3+k][j*3+l]] = 1;
 						}
 					}
 					for (int k = 1; k <= 9; k++) {
-						if(num[k] != 1) {
+						if(num[k] != 1) { 
 							room = true;
 							break;
 						}
