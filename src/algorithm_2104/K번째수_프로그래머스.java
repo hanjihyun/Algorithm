656a05d8d399a1da0a5816c63a08fef7dfea33f3
한지현 <gksgpals96@naver.com>
2021-04-14 23:17:35 +0900

    Feat: K번째 수 프로그래머스

---
+++ src/algorithm_2104/K번째수_프로그래머스.java
@@ -0,0 +1,38 @@
+package algorithm_2104;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class K번째수_프로그래머스 {
+
+	public static void main(String[] args) {
+		int[] array = { 1, 5, 2, 6, 3, 7, 4 };
+		int[][] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+		int[] ar = solution(array, commands);
+
+		System.out.println();
+	}
+
+	public static int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		
+		List list = new ArrayList<Integer>();
+		for (int i = 0; i < commands.length; i++) {
+			list = new ArrayList<Integer>();
+
+			for (int k = commands[i][0] - 1; k <= commands[i][1] - 1; k++) {
+				list.add(array[k]);
+			}
+			Collections.sort(list);
+//			for (int j = 0; j < list.size(); j++) {
+//
+//				System.out.println(list.toString());
+//			}
+			answer[i] = (int) list.get(commands[i][2]-1);
+		
+		}
+	
+		return answer;
+	}
+}
