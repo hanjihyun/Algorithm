dee6dae1e294236a75e805639cb71f97deb79dee
한지현 <gksgpals96@naver.com>
2021-07-08 19:50:04 +0900

    Fix : new solution

--- src/algorithm_2107/최댓값과최솟값_Pro.java
+++ src/algorithm_2107/최댓값과최솟값_Pro.java
@@ -5,31 +5,41 @@ import java.util.ArrayList;
 public class 최댓값과최솟값_Pro {
 	public static void main(String[] args) {
 		String s = "-1 -2 -3 -4";
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		String str = "";
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == ' ') {
-				list.add(Integer.parseInt(str));
-				str = "";
-			} else {
-				str += s.charAt(i);
-//				System.out.println(str);
-			} 
-		}
-		list.add(Integer.parseInt(str));
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
+//		ArrayList<Integer> list = new ArrayList<Integer>();
+//		String str = "";
+//		for (int i = 0; i < s.length(); i++) {
+//			if (s.charAt(i) == ' ') {
+//				list.add(Integer.parseInt(str));
+//				str = "";
+//			} else {
+//				str += s.charAt(i);
+////				System.out.println(str);
+//			} 
+//		}
+//		list.add(Integer.parseInt(str));
+//		int min = Integer.MAX_VALUE;
+//		int max = Integer.MIN_VALUE;
+//		
+//		for (int i = 0; i < list.size(); i++) {
+//			if(list.get(i)<min) {
+//				min = list.get(i);
+//			}
+//			if(list.get(i)>max) {
+//				max = list.get(i);
+//			}
+////			System.out.println(list.get(i));
+//		}
+//		String answer = min + " " + max;
 		
-		for (int i = 0; i < list.size(); i++) {
-			if(list.get(i)<min) {
-				min = list.get(i);
-			}
-			if(list.get(i)>max) {
-				max = list.get(i);
-			}
-//			System.out.println(list.get(i));
-		}
-		String answer = min + " " + max;
+		String[] tmp = s.split(" ");
+        int min, max, n;
+        min = max = Integer.parseInt(tmp[0]);
+        for (int i = 1; i < tmp.length; i++) {
+                n = Integer.parseInt(tmp[i]);
+            if(min > n) min = n;
+            if(max < n) max = n;
+        }
+        String answer = min + " " + max;
 		System.out.println(answer);
 	}
 }
