2fad13511078383c0ca3b5113660105a03df713a
한지현 <gksgpals96@naver.com>
2021-07-14 14:02:45 +0900

    Fix : reSolve

--- src/algorithm_2107/프린터_Pro.java
+++ src/algorithm_2107/프린터_Pro.java
@@ -1,46 +1,31 @@
 package algorithm_2107;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class 프린터_Pro {
 	public static void main(String[] args) {
-		int[] priorities = {1, 1, 3, 2, 1, 4, 1, 1, 3, 1, 1, 1};
-		int location = 8;
+		int[] priorities = {2, 1, 3, 2};
+		int location = 2;
 		int answer = 0;
-
-		int loca = location;
-		boolean chk = false;
-		
-		ArrayList<Integer> qu = new ArrayList<Integer>();
-		for (int i = 0; i < priorities.length; i++) {
-			int cnt = 0;
-			for (int j = i + 1; j < priorities.length; j++) {
-				if (priorities[i] < priorities[j]) {
-					cnt++;
-				}
-			}
-			if (cnt != 0) {
-				qu.add(priorities[i]);
-				if (i == location) {
-					loca = qu.size() - 1;
-				}
-
-			} else {
-				if (i == location) {
-					answer++;
-					chk = true;
-					break;
-				} else {
-					answer++;
-				}
-			}
-
-		}
-		if (!chk) {
-			for (int i = 0; i <= loca; i++) {
-				answer++;
-			}
-		}
+		PriorityQueue<Integer> prior = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int number:priorities){
+            prior.offer(number);
+        }
+        while(!prior.isEmpty()){
+            for(int i = 0; i < priorities.length; i++){
+                if(prior.peek() == priorities[i]){
+                    prior.poll();
+                    answer++;
+                    
+                    if(location == i){
+                        prior.clear();
+                        break;
+                    }
+                }
+            }
+        }
 		System.out.println(answer);
 	}
 }
