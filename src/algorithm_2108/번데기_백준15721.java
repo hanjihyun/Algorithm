bb2c46a94d4d97feda76fd1e714b2ca33a998491
지현 <gksgpals96@naver.com>
2021-08-16 22:33:55 +0900

    Update and rename 번데기_백준.java to 번데기_백준15721.java

---
+++ src/algorithm_2108/번데기_백준15721.java
@@ -0,0 +1,15 @@
+package algorithm_2108;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class 번데기_백준15721 {
+	static int A, T;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		A = sc.nextInt();
+		T = sc.nextInt();
+	}
+}
