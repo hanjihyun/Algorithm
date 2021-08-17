d8670bdf221f68b0b987384558cbf47f0372edec
지현 <gksgpals96@naver.com>
2021-08-17 13:59:06 +0900

    Update 번데기_백준15721.java

--- src/algorithm_2108/번데기_백준15721.java
+++ src/algorithm_2108/번데기_백준15721.java
@@ -1,15 +1,49 @@
 package algorithm_2108;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class 번데기_백준15721 {
-	static int A, T;
+	static int A, T, what;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		A = sc.nextInt();
 		T = sc.nextInt();
+		what = sc.nextInt();
+
+		int Ans = sol();
+		System.out.println(Ans);
+	}
+
+	private static int sol() {
+		int bbun = 0;
+		int degi = 0;
+		int num = 2; // 1회차에 두번 반복
+
+		while (true) {
+			for (int i = 0; i < 4; i++) {
+				if (i % 2 == 0)
+					bbun++;
+				else
+					degi++;
+				if (bbun == T && what == 0)
+					return (bbun + degi - 1) % A;
+				if (degi == T && what == 1)
+					return (bbun + degi - 1) % A;
+			}
+
+			for (int i = 0; i < num; i++) {
+				bbun++;
+				if (bbun == T && what == 0)
+					return (bbun + degi - 1) % A;
+			}
+			for (int i = 0; i < num; i++) {
+				degi++;
+				if (degi == T && what == 1)
+					return (bbun + degi - 1) % A;
+			}
+			num++; // 회차 증가
+		}
 	}
 }
