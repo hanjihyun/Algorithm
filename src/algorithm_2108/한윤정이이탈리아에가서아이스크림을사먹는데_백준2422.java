faf4c90e00e6fbeb0f59d08960ac822ebac47478
한지현 <gksgpals96@naver.com>
2021-08-19 11:53:02 +0900

    Fix : solution

--- src/algorithm_2108/한윤정이이탈리아에가서아이스크림을사먹는데_백준2422.java
+++ src/algorithm_2108/한윤정이이탈리아에가서아이스크림을사먹는데_백준2422.java
@@ -1,77 +1,59 @@
 package algorithm_2108;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class 한윤정이이탈리아에가서아이스크림을사먹는데_백준2422 {
 	static int N, M, Ans;
-	static List<combiX> list;
-	static int[] sel, arr;
-
-	static class combiX {
-		int a, b;
-
-		combiX(int a, int b) {
-			super();
-			this.a = a;
-			this.b = b;
-		}
-
-	}
+	static int[] sel;
+	static boolean[][] pairs;
+	static boolean[] v;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		N = sc.nextInt();
 		M = sc.nextInt();
-		list = new ArrayList<>();
-		arr = new int[N];
 		sel = new int[3];
-
-		for (int i = 0; i < N; i++) {
-			arr[i] = i + 1;
-		}
+		pairs = new boolean[N][N];
+		v = new boolean[N];
 		for (int i = 0; i < M; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			list.add(new combiX(a, b));
+			int a = sc.nextInt() - 1;
+			int b = sc.nextInt() - 1;
+			pairs[a][b] = true;
+			pairs[b][a] = true;
 		}
+		Ans = 0;
 		combiO(0, 0);
+
 		System.out.println(Ans);
 	}
 
 	private static void combiO(int idx, int k) {
-		if (sel.length == k) {
-			boolean chk = false;
+		if (k == 3) {
+			boolean chk = true;
 
-			for (int j = 0; j < list.size(); j++) {
-				int cnt = 0;
-				for (int i = 0; i < sel.length; i++) {
-
-					if (sel[i] == list.get(j).a) {
-						cnt++;
-					} else if (sel[i] == list.get(j).b) {
-						cnt++;
-					}
-					if (cnt >= 2) {
-						chk = true;
-						break;
-					}
+			for (int i = 0; i < 2; i++) {
+				for (int j = i + 1; j < 3; j++) {
+					if (pairs[sel[i]][sel[j]])
+						chk = false;
 				}
 			}
-			if (chk == false) {
+
+			if (chk == true) {
 				Ans++;
 				System.out.println(Arrays.toString(sel));
 			}
 			return;
 		}
-		if (idx == arr.length) {
-			return;
+
+		for (int i = idx; i < N; i++) {
+			if (!v[i]) {
+				v[i] = true;
+				sel[k] = i;
+				combiO(i, k + 1);
+				v[i] = false;
+			}
 		}
-		sel[k] = arr[idx];
-		combiO(idx + 1, k + 1);
-		combiO(idx + 1, k);
 	}
 }
