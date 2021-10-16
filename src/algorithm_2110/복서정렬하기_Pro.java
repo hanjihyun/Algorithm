6f090ef8baebfe2c0b33fca835a70806a2e0c38d
한지현 <gksgpals96@naver.com>
2021-10-16 16:17:18 +0900

    Add : algo

---
+++ src/algorithm_2110/복서정렬하기_Pro.java
@@ -0,0 +1,20 @@
+package algorithm_2110;
+
+public class 복서정렬하기_Pro {
+public static void main(String[] args) {
+//	 int[] answer = new int[weights.length];
+//     double[] win = new double[weights.length];
+//     int total =weights.length, winC=0;
+//     for(int i=0; i<weights.length; i++){
+//         for(int j=0; j<head2head.length; j++){
+//             if(head2head[i].charAt(j) == 'N') total--;
+//             else if(head2head[i].charAt(j) == 'L')winC++;
+//         }
+//          System.out.println(total + " "+winC);
+//         // win[i] = 100 * (winC/total);
+//     }
+//      for(int i=0; i<win.length; i++){
+//         System.out.println(win[i]);
+//      }
+}
+}
