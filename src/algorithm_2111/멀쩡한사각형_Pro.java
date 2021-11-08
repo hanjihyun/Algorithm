31341ca3d8b37de7f0cf2f9d41220fd6c2e34672
한지현 <gksgpals96@naver.com>
2021-11-08 12:35:02 +0900

    Fix : algo

--- src/algorithm_2111/멀쩡한사각형_Pro.java
+++ src/algorithm_2111/멀쩡한사각형_Pro.java
@@ -1,5 +1,28 @@
 package algorithm_2111;
 
 public class 멀쩡한사각형_Pro {
+	public static void main(String[] args) {
+		int w = 8;
+		int h = 12;
+		long wei = w;
+		long hei = h;
+		long answer = wei * hei;
+		long g = gcd(wei, hei);
+		wei /= g;
+		hei /= g;
+		answer = answer - g * (wei + hei - 1);
+		System.out.println(answer);
+	}
 
+	static long gcd(long a, long b) { // 최대공약수
+
+		long x = Math.max(a, b);
+		long y = Math.min(a, b);
+		while (y != 0) {
+			long r = x % y;
+			x = y;
+			y = r;
+		}
+		return x;
+	}
 }
