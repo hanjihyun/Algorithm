8d712b2bc0ab8b911430f6a6c11bab03bc44659d
한지현 <gksgpals96@naver.com>
2022-02-28 23:58:38 +0900

    Fix : solution

--- src/algorithm_2202/뉴스클러스터링_Pro.java
+++ src/algorithm_2202/뉴스클러스터링_Pro.java
@@ -1,6 +1,7 @@
 package algorithm_2202;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class 뉴스클러스터링_Pro {
@@ -17,37 +18,49 @@ public class 뉴스클러스터링_Pro {
 		List<String> inter = new ArrayList();
 		List<String> comb = new ArrayList();
 
-		for (int i = 0; i < str1.length() - 1; i++) {
-			char first = str1.charAt(i);
-			char second = str1.charAt(i + 1);
-			if (first >= 'a' && first <= 'z' && second >= 'a' && second <= 'z') {
-				arr1.add(first + "" + second);
-			}
-		}
-
-		for (int i = 0; i < str2.length() - 1; i++) {
-			char first = str2.charAt(i);
-			char second = str2.charAt(i + 1);
-			if (first >= 'a' && first <= 'z' && second >= 'a' && second <= 'z') {
-				arr2.add(first + "" + second);
-			}
-		}
-
-		for (String s : arr1) {
-			if (arr2.remove(s)) {
-				inter.add(s);
-			}
-			comb.add(s);
-		}
-
-		comb.addAll(arr2);
-
-		double gong_len = inter.size();
-		double hab_len = comb.size();
-		if (hab_len == 0) {
-			answer = 65536;
-		}
-		answer = (int) (gong_len / hab_len * 65536);
+		for(int i = 0 ; i < str1.length() - 1 ; ++i){
+	          char first = str1.charAt(i);
+	          char second = str1.charAt(i + 1);
+	          
+	          if(first >= 'a' && first <= 'z' &&
+	             second >= 'a' && second <= 'z'){
+	              arr1.add(first + "" + second);
+	          }
+	      }
+	      
+	      for(int i = 0 ; i < str2.length() - 1 ; ++i){
+	          char first = str2.charAt(i);
+	          char second = str2.charAt(i + 1);
+	          
+	          if(first >= 'a' && first <= 'z' &&
+	             second >= 'a' && second <= 'z'){
+	              arr2.add(first + "" + second);
+	          }
+	      }
+	      
+	      Collections.sort(arr1);
+	      Collections.sort(arr2);
+	            
+	      for(String s : arr1){
+	          if(arr2.remove(s)){
+	            comb.add(s);
+	          }
+	          inter.add(s);
+	      }
+	      
+	      for(String s : arr2){
+	          inter.add(s);
+	      }
+	      
+	      double jakard = 0;
+	      
+	      if(inter.size() == 0) {
+	          jakard = 1;
+	      } else {
+	          jakard = (double)comb.size() / (double)inter.size();
+	      }
+	            
+	      answer = (int)(jakard * 65536);
 		System.out.println(answer);
 	}
 }
