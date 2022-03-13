ae0f9b4eca6ba39d64791d2842b48ba9712f0033
한지현 <gksgpals96@naver.com>
2022-03-13 22:15:29 +0900

    Fix : algo

--- src/algorithm_2203/이진변환반복하기_Pro.java
+++ src/algorithm_2203/이진변환반복하기_Pro.java
@@ -1,5 +1,30 @@
 package algorithm_2203;
 
 public class 이진변환반복하기_Pro {
-
+	public static void main(String[] args) {
+		String s = "110010101001";
+		int[] answer = new int[2];
+		
+		int cnt = 0;
+		int cnt_z = 0;
+		
+		while(!s.equals("1")) {
+			int cnt_o = 0;
+			for (int i = 0; i < s.length(); i++) {
+				if(s.charAt(i)=='1') {
+					cnt_o++;
+				}else {
+					cnt_z++;
+				}
+			}
+			s = Integer.toBinaryString(cnt_o);
+			cnt++;
+		}
+		answer[0] = cnt;
+		answer[1] = cnt_z;
+		
+		for (int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i]+" ");
+		}
+	}
 }
